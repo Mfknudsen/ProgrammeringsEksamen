5a1821611fa9104d95695dcd16172db35d2165f5
Mfknudsen <35879356+Mfknudsen@users.noreply.github.com>
2020-03-06 11:58:39 +0100

    +

---
+++ Programmerings Eksamen/Assets/_Scripts/Menu/Exit.cs
@@ -0,0 +1,13 @@
+﻿#region Systems
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#endregion
+
+public class Exit : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Programmerings Eksamen/Assets/_Scripts/Menu/StartGame.cs
@@ -0,0 +1,13 @@
+﻿#region Systems
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#endregion
+
+public class StartGame : MonoBehaviour
+{
+    public void StartFreshGame(){
+        //SceneManagement.LoadScene("Main", LoadSceneMode.Additive);
+    }
+}
